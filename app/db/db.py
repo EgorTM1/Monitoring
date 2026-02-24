48daaff548b5be1b9dda52a8e0377d0cec676666
Egor <tolmacevegor46@gmail.com>
2026-02-25 00:48:37 +0300

    Initial commit

---
+++ app/db/db.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+from settings import settings
+
+
+engine = create_async_engine(
+    url=settings.get_dsn,
+    echo=True
+)
+
+session_factory = async_sessionmaker(
+    bind=engine,
+    autoflush=False,
+    expire_on_commit=False
+)
+
+class Base(DeclarativeBase):
+    pass
